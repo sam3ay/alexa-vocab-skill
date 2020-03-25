160b184ec2cccf368a0f3c510b257f82e7d4fe1e
Samuel Addo-Yobo <30203976+sam3ay@users.noreply.github.com>
2020-03-25 21:43:09 +0000

    word-search added test renamed

--- lambda/__tests__/word-search.test.js
+++ lambda/__tests__/word-search.test.js
@@ -4,13 +4,13 @@ const fc = require("fast-check");
 describe('Word Lookup', () => {
 	it("Given an invalid word, return an error(404) message", async () => {
 		expect.assertions(1);
-		error_msg = await add_word.lookUpWord("notaword");
+		error_msg = await lookUpWord("notaword");
 		expect(error_msg).toEqual("/404/")
 	});
 	it("Given a valid word, retrieve list of nested lists with definitions and part of speech", async () => {
 		expect.assertions(1);
 		// mock and fast-check for rigorous testing
-		let definitions = await add_word.retrieveDefinition(mockjson);
+		let definitions = await retrieveDefinition(mockjson);
 		expect(definitions).toEqual([]);
 	});
 });
