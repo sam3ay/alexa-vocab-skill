11ee29c7b10f1450b357a889d9b6dfd2009a71c0
Samuel Addo-Yobo <30203976+sam3ay@users.noreply.github.com>
2020-03-28 05:55:33 +0000

    retrieve definition refactor(trim)

--- lambda/__tests__/word-search.test.js
+++ lambda/__tests__/word-search.test.js
@@ -1,6 +1,379 @@
 const app = require("..");
 const fc = require("fast-check");
-//
+const got = jest.genMockOFromModule('got');
+const nock = require("nock");
+
+results = [
+	{
+		"id": "bear",
+		"language": "en-gb",
+		"lexicalEntries": [
+			{
+				"entries": [
+					{
+						"homographNumber": "100",
+						"senses": [
+							{
+								"definitions": [
+									"(of a person) carry"
+								],
+								"id": "m_en_gbus0080800.012",
+								"subsenses": [
+									{
+										"definitions": [
+											"(of a vehicle or boat) convey (passengers or cargo)"
+										],
+										"id": "m_en_gbus0080800.018"
+									},
+									{
+										"definitions": [
+											"have or display as a visible mark or feature"
+										],
+										"id": "m_en_gbus0080800.019"
+									},
+									{
+										"definitions": [
+											"be called by (a name or title)"
+										],
+										"id": "m_en_gbus0080800.020"
+									},
+									{
+										"definitions": [
+											"carry or conduct oneself in a specified manner"
+										],
+										"id": "m_en_gbus0080800.021"
+									}
+								]
+							},
+							{
+								"definitions": [
+									"support; carry the weight of"
+								],
+								"id": "m_en_gbus0080800.023",
+								"subsenses": [
+									{
+										"definitions": [
+											"take responsibility for"
+										],
+										"id": "m_en_gbus0080800.024"
+									},
+									{
+										"definitions": [
+											"be able to accept or stand up to"
+										],
+										"id": "m_en_gbus0080800.025"
+									}
+								]
+							},
+							{
+								"definitions": [
+									"endure (an ordeal or difficulty)"
+								],
+								"id": "m_en_gbus0080800.027",
+								"subsenses": [
+									{
+										"definitions": [
+											"manage to tolerate (a situation or experience)"
+										],
+										"id": "m_en_gbus0080800.028"
+									},
+									{
+										"definitions": [
+											"strongly dislike"
+										],
+										"id": "m_en_gbus0080800.029"
+									}
+								]
+							},
+							{
+								"definitions": [
+									"give birth to (a child)"
+								],
+								"id": "m_en_gbus0080800.031",
+								"subsenses": [
+									{
+										"definitions": [
+											"(of a tree or plant) produce (fruit or flowers)"
+										],
+										"id": "m_en_gbus0080800.032"
+									}
+								]
+							},
+							{
+								"definitions": [
+									"turn and proceed in a specified direction"
+								],
+								"id": "m_en_gbus0080800.034"
+							}
+						]
+					}
+				],
+				"language": "en-gb",
+				"lexicalCategory": {
+					"id": "verb",
+					"text": "Verb"
+				},
+				"text": "bear"
+			}
+		],
+		"type": "headword",
+		"word": "bear"
+	},
+	{
+		"id": "bear",
+		"language": "en-gb",
+		"lexicalEntries": [
+			{
+				"entries": [
+					{
+						"homographNumber": "200",
+						"senses": [
+							{
+								"definitions": [
+									"a large, heavy mammal that walks on the soles of its feet, having thick fur and a very short tail. Bears are related to the dog family but most species are omnivorous."
+								],
+								"id": "m_en_gbus0080810.006",
+								"subsenses": [
+									{
+										"definitions": [
+											"a teddy bear."
+										],
+										"id": "m_en_gbus0080810.010"
+									},
+									{
+										"definitions": [
+											"a nickname for Russia."
+										],
+										"id": "m_en_gbus0080810.011"
+									}
+								]
+							},
+							{
+								"definitions": [
+									"a large, heavy, cumbersome man"
+								],
+								"id": "m_en_gbus0080810.017",
+								"subsenses": [
+									{
+										"definitions": [
+											"a gay or bisexual man with a burly physique and a large amount of body hair"
+										],
+										"id": "m_en_gbus0080810.018"
+									},
+									{
+										"definitions": [
+											"a rough, bad-mannered, or uncouth person"
+										],
+										"id": "m_en_gbus0080810.019"
+									},
+									{
+										"definitions": [
+											"something that is very difficult or unpleasant to deal with"
+										],
+										"id": "m_en_gbus0080810.020"
+									}
+								]
+							},
+							{
+								"definitions": [
+									"a person who sells shares hoping to buy them back later at a lower price."
+								],
+								"id": "m_en_gbus0080810.022"
+							}
+						]
+					}
+				],
+				"language": "en-gb",
+				"lexicalCategory": {
+					"id": "noun",
+					"text": "Noun"
+				},
+				"text": "bear"
+			}
+		],
+		"type": "headword",
+		"word": "bear"
+	}
+]
+
+function generateResponse(word, randdef1, randdef2, randdef3) {
+	header_ret = {
+		"api_version": "v2",
+		"code_version": "leap2-v2.6.0-ge749fd1",
+		"connection": "keep-alive",
+		"content-encoding": "gzip",
+		"content-length": "918",
+		"content-type": "application/json;charset=utf-8",
+		"date": "Fri, 27 Mar 2020 20:58:21 GMT",
+		"server": "openresty/1.17.4.1rc0",
+		"x-request-id": "3ded642bc492d929c67d1e79b1c262d0"
+	};
+	response_body = {
+		"id": word,
+		"metadata": {
+			"operation": "retrieve",
+			"provider": "Oxford University Press",
+			"schema": "RetrieveEntry"
+		},
+		"results": [
+			{
+				"id": word,
+				"language": "en-gb",
+				"lexicalEntries": [
+					{
+						"entries": [
+							{
+								"homographNumber": "100",
+								"senses": [
+									{
+										"definitions": [
+											randdef1
+										],
+										"id": "m_en_gbus0572960.007"
+									},
+									{
+										"definitions": [
+											"relating to a person or group favouring radical, reforming, or socialist views; left wing"
+										],
+										"id": "m_en_gbus0572960.012"
+									}
+								]
+							}
+						],
+						"language": "en-gb",
+						"lexicalCategory": {
+							"id": "adjective",
+							"text": "Adjective"
+						},
+						"text": word
+					},
+					{
+						"entries": [
+							{
+								"homographNumber": "101",
+								"senses": [
+									{
+										"definitions": [
+											"on or to the left side"
+										],
+										"id": "m_en_gbus0572960.014"
+									}
+								]
+							}
+						],
+						"language": "en-gb",
+						"lexicalCategory": {
+							"id": "adverb",
+							"text": "Adverb"
+						},
+						"text": word
+					},
+					{
+						"entries": [
+							{
+								"homographNumber": "102",
+								"senses": [
+									{
+										"definitions": [
+											randdef2
+										],
+										"id": "m_en_gbus0572960.018",
+										"subsenses": [
+											{
+												"definitions": [
+													"(in soccer or a similar sport) the left-hand half of the field when facing the opponents' goal"
+												],
+												"id": "m_en_gbus0572960.020"
+											},
+											{
+												"definitions": [
+													randdef3
+												],
+												"id": "m_en_gbus0572960.022"
+											},
+											{
+												"definitions": [
+													"a left turn"
+												],
+												"id": "m_en_gbus0572960.023"
+											},
+											{
+												"definitions": [
+													"a road, entrance, etc. on the left"
+												],
+												"id": "m_en_gbus0572960.026"
+											},
+											{
+												"definitions": [
+													"a person's left fist, especially a boxer's"
+												],
+												"id": "m_en_gbus0572960.027"
+											},
+											{
+												"definitions": [
+													"a blow given with the left fist"
+												],
+												"id": "m_en_gbus0572960.028"
+											}
+										]
+									},
+									{
+										"definitions": [
+											"a group or party favouring radical, reforming, or socialist views"
+										],
+										"id": "m_en_gbus0572960.030"
+									}
+								]
+							}
+						],
+						"language": "en-gb",
+						"lexicalCategory": {
+							"id": "noun",
+							"text": "Noun"
+						},
+						"text": word
+					}
+				],
+				"type": "headword",
+				"word": word
+			},
+			{
+				"id": word,
+				"language": "en-gb",
+				"lexicalEntries": [
+					{
+						"entries": [
+							{
+								"homographNumber": "200"
+							}
+						],
+						"language": "en-gb",
+						"lexicalCategory": {
+							"id": "verb",
+							"text": "Verb"
+						},
+						"text": word
+					}
+				],
+				"type": "headword",
+				"word": word
+			}
+		],
+		"word": word
+	}
+	const response = nock("od-api.oxforddictionaries.com", {
+		reqheaders: {
+			'app_id': "app_id",
+			'app_key': "app_key",
+		},
+	})
+		.get(`/api/v2/entries/en-gb/${word}?fields=definitions&strictMatch=True`)
+		.defaultReplyHeader(
+			header_ret
+		)
+		.reply(200, response_body)
+}
+// Integration Test, Looking up a word in a dictionary
 describe('Word Lookup', () => {
 	it("Given an invalid word, return an error(404) message", async () => {
 		expect.assertions(1);
@@ -9,8 +382,23 @@ describe('Word Lookup', () => {
 	});
 	it("Given a valid word, retrieve list of nested lists with definitions and part of speech", async () => {
 		expect.assertions(1);
-		// mock and fast-check for rigorous testing
-		let definitions = await retrieveDefinition(mockjson);
-		expect(definitions).toEqual([]);
+		fc.assert(
+			fc.asyncProperty(
+				fc.string(30),
+				fc.lorem(15, true),
+				fc.lorem(25, true),
+				fc.lorem(35, true),
+				(word, def1, def2, def3) => {
+					generateResponse(word, def1, def2, def3);
+					definitions = [def1, def2, def3,];
+					expect(lookUpWord(word)).toEqual(definitions);
+				}
+			)
+		)
 	});
 });
+
+describe('Word Search', () => {
+	it("Sending a correctly structured request") {
+	}
+})
--- lambda/libs/word-search.js
+++ lambda/libs/word-search.js
@@ -6,7 +6,21 @@ const ErrorHandler = require("../handler/errorhandler.js")
 	* @param {string} word search parameter
 	* @param {object} credentials contains id and key for oxford api
 	*/
-async function lookUpWord(word) {
+function lookUpWord(word) {
+	try {
+		results = searchWord(word)
+		definitions = retrieveDefinition(results);
+		return definitions;
+	} catch (error) {
+		return ErrorHandler(error);
+	};
+};
+
+/**
+ * Returns an array of objects containing information about the word
+ * @param {string} word word to find in dictionary
+ */
+async function searchWord(word) {
 	let app_id = process.env.oxford_app_id;	// get this from the oxford dictionary api webpage
 	let app_key = process.env.oxford_app_key; // get this from the oxford dictionary api webpage
 	// determines fields returned strict maching prevents homographs
@@ -23,18 +37,13 @@ async function lookUpWord(word) {
 	};
 	try {
 		let response = await got("https://od-api.oxforddictionaries.com", options).json();
-		console.log(response);
 		// main_def = test_get.results[0].lexicalEntries[0].entries[0].senses[0].definitions[0]
 		results = response.results;
-		definitions = retrieveDefinition(results);
-		return definitions;
-	} catch (error) {
+		return results
+	} catch {
 		return ErrorHandler(error);
 	}
-};
-
-
-
+}
 /**
  * Extracts information from array of objects 
  * @param {array} response_array Nested array of objects
@@ -42,38 +51,38 @@ async function lookUpWord(word) {
  * @param {string} identifier Value being searched
  */
 function retrieveDefinition(response_array, metadata_path = "lexicalCategory", identifier = "definitions") {
-	var definitions = []
+	var definition_list = []
 	function parseResult(response_array, metadata) {
 		// Confirm object type
-		if (Array.isArray(response_array)) {
+		try {
 			response_array.forEach((item) => {
 				// lexicalEntries provides part of speech for all entries attributes
 				if (item[metadata_path]) {
 					metadata = item[metadata_path].text
-					// Iterate over properties items
-					for (element in item) {
-						parseResult(item[element], metadata)
-					}
 				} else if (item[identifier]) {
-					// Update definitions array with list of metadata and definition
-					definitions.push([metadata, item[identifier][0]]);
-				} else if (Array.isArray(item)) {
-					// Iterate over array # check logic
-					parseResult(item, metadata);
-				} else if (typeof item === 'object' && item !== null) {
+					// update definitions array with list of metadata and definition
+					definition_list.push([metadata, item[identifier][0]]);
+				}
+				if (typeof item === 'object' && item !== null) {
 					// If item is a non-null object, (therefore not a primitive)
 					// Iterate over properties items # Could be exported
 					for (element in item) {
 						parseResult(item[element], metadata)
 					}
 				}
+				parseResult(item, metadata);
 			})
+		} catch (error) {
+			if (error == "TypeError") {
+				// Primitive/null encountered
+			}
 		}
 	}
 	try {
 		parseResult(response_array)
-		return definitions
+		return definition_list
 	} catch (error) {
+		console.log(error)
 		return ErrorHandler(error)
 	}
 }
