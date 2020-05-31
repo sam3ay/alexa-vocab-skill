1ec34a67237bc98db84d11d383a0e007334856be
Samuel Addo-Yobo <30203976+sam3ay@users.noreply.github.com>
2020-05-31 05:39:52 +0000

    sagemaker endpoint helper added

--- lambda/libs/flashcard-helper.mjs
+++ lambda/libs/flashcard-helper.mjs
@@ -1,13 +1,13 @@
 import _ from 'lodash';
-
+import sagemaker from '@aws-sdk/client-sagemaker-runtime'
 
 /**
  * 
  * @param {array} keys array of strings
  */
 function randomUnknownWord(keys) {
-	// << floor division sub
-	return keys.splice([keys.length * Math.random() << 0], 1);
+    // << floor division sub
+    return keys.splice([keys.length * Math.random() << 0], 1);
 };
 
 /**
@@ -15,12 +15,12 @@ function randomUnknownWord(keys) {
  * @param {object} flashCards object with words as strings and definitions as arrays
  */
 function getFlashcard(flashCards, keys) {
-	if (keys === undefined) {
-		keys = Object.keys(flashCards);
-	};
-	let [word] = randomUnknownWord(keys);
-	let definitions = flashCards[word].reduce((acc, val) => acc.concat(val[1]), []);
-	return [word, definitions, keys];
+    if (keys === undefined) {
+        keys = Object.keys(flashCards);
+    };
+    let [word] = randomUnknownWord(keys);
+    let definitions = flashCards[word].reduce((acc, val) => acc.concat(val[1]), []);
+    return [word, definitions, keys];
 }
 
 /**
@@ -28,9 +28,9 @@ function getFlashcard(flashCards, keys) {
  * @param {object} flashCards object with words as strings and definitions as arrays
  */
 function getQuestion(flashCards, keys = undefined) {
-	let [word, semanticDef, wordlist] = getFlashcard(flashCards, keys);
-	let speech = `What is the definition of ${word}?`;
-	return [speech, word, semanticDef, wordlist]
+    let [word, semanticDef, wordlist] = getFlashcard(flashCards, keys);
+    let speech = `What is the definition of ${word}?`;
+    return [speech, word, semanticDef, wordlist];
 }
 
 /**
@@ -38,40 +38,49 @@ function getQuestion(flashCards, keys = undefined) {
  * @param valueArray depth 0 list of strings
  */
 function createSlotValues(valueArray) {
-	let outArray = []
-	for (let i = 0; i < valueArray.length; i++) {
-		outArray.push({
-			id: `${i}`,
-			name: {
-				value: valueArray[i]
-			}
-		})
-	}
-	return outArray
+    let outArray = []
+    for (let i = 0; i < valueArray.length; i++) {
+        outArray.push({
+            id: `${i}`,
+            name: {
+                value: valueArray[i]
+            }
+        });
+    };
+    return outArray
 };
 
-function testDefinition(answer, definition) {
-	if (answer == 'right') {
-		return [true, definition[0]];
-	} else {
-		return [false, definition[1]];
-	};
+async function testDefinition(answer, definition) {
+    let endpointName = process.env.ENDPOINT_NAME;	// get this from the oxford dictionary api webpage
+    let body = JSON.stringify({
+        query: [answer],
+        corpus: definition
+    });
+    let params = {
+        EndpointName: endpointName,
+        Body: body,
+        ContentType: 'application/json',
+        Accept: 'application/json'
+    };
+    const client = new sagemaker.SageMakerRuntime();
+    let response = await client.invokeEndpoint(params);
+    return response;
 };
 
 function checkDeck(flashCards, word) {
-	if (_.has(flashCards, `words.unknownWords.${word}`) || _.has(flashCards, `words.knownWords.${word}`)) {
-		return true;
-	} else {
-		return false;
-	};
+    if (_.has(flashCards, `words.unknownWords.${word}`) || _.has(flashCards, `words.knownWords.${word}`)) {
+        return true;
+    } else {
+        return false;
+    };
 };
 
 
 export {
-	checkDeck,
-	testDefinition,
-	createSlotValues,
-	getFlashcard,
-	getQuestion,
-	randomUnknownWord
+    checkDeck,
+    testDefinition,
+    createSlotValues,
+    getFlashcard,
+    getQuestion,
+    randomUnknownWord
 }
